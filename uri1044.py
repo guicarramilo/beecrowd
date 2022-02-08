25d46d6568e704c33b26dae7dc7573157a369409
guicarramilo <84037040+guicarramilo@users.noreply.github.com>
2022-02-08 13:04:15 -0300

    Delete uri1044.py

--- uri1044.py
+++
@@ -1,9 +0,0 @@
-def multiplos():
-    A, B= map(float, input().split())
-    if B % A == 0 or A % B == 0:
-        print('Sao Multiplos')
-    else:
-        print('Nao sao Multiplos')
-
-multiplos()
-multiplos()
